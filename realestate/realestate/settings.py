d7f7fb2764b65da2c7508e822fe87966d17e1616
john22-sarker <johnsarker790@gmail.com>
2026-03-30 08:21:55 -0700

    Update settings.py

--- realestate/realestate/settings.py
+++ realestate/realestate/settings.py
@@ -211,3 +211,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # ===============================
 DATA_UPLOAD_MAX_MEMORY_SIZE = 10485760
 FILE_UPLOAD_MAX_MEMORY_SIZE = 10485760
+
+
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'your_email@gmail.com'
+EMAIL_HOST_PASSWORD = 'your_app_password'
