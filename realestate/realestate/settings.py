ba4e38170390480822d57b8149af95ec5cc15bda
john22-sarker <johnsarker790@gmail.com>
2026-03-30 09:54:26 -0700

    Update settings.py

--- realestate/realestate/settings.py
+++ realestate/realestate/settings.py
@@ -220,4 +220,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'johnsarker790@gmail.com'
-EMAIL_HOST_PASSWORD = 'nozfhzrhoadvzcxk'
+EMAIL_HOST_PASSWORD = ''
