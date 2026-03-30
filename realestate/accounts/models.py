ba8aa4d28632b90c712f410ae4ab0c4f2dd25e32
john22-sarker <johnsarker790@gmail.com>
2026-03-30 08:12:58 -0700

    Update models.py

--- realestate/accounts/models.py
+++ realestate/accounts/models.py
@@ -2,6 +2,17 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+import random
+
+class EmailOTP(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    otp = models.CharField(max_length=6)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def generate_otp(self):
+        self.otp = str(random.randint(100000, 999999))
+        self.save()
+
 
 
 class UserProfile(models.Model):
